3a1e2328ad77f3cbb499eb96ede91bf44db33996
2025138125 <chanhee2018@naver.com>
2026-04-29 16:49:41 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,17 +3,19 @@
 int main()
 {
    int n =0, a=0, b=0, c=0;
+   bool i=false;
    scanf("%d", &n);
-   for(a=1, n; a<=n/900; a++) {
-        for(b=2, n; b<=n/750; b+=2) {
-            for(c=1; c<=n/200 && c<b || c<a; c++) {
+   for(a=1; a<=n/900; a++) {
+        for(b=2; b<=n/750; b+=2) {
+            for(c=1; c<=n/200 && (c<b || c<a); c++) {
                 if(n==900*a+750*b+c*200) {
                 printf("%d %d %d\n", a,b,c);
-        }   
-   }
+                i = true;
+            }   
+        }
+    }
 }
-}
-if(n!=900*a+750*b+c*200) {
+if(i == false) {
     printf("none");
 }
 }
